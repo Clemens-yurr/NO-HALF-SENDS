6f6c2a208f5a9d413a211718f87e0772c58d43d9
Clemens Deutinger <lahmaschtv@gmail.com>
2026-04-08 10:59:15 +0200

    files

---
+++ NO_HALF_SENDS/src/main/resources/assets/levels/Road_Main_Straight.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Road_Main_Straight" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="../../../../../../../../../OneDrive - HTL Saalfelden/NO HALF SENDS/Race-Track-Tile-Set-PNG/PNG/Road_01/Road_01_Tile_03/Layers/Road_Main_Straight.png" width="512" height="512"/>
+</tileset>
